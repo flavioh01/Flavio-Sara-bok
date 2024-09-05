354106d6880ecacf36fa346d0ceca40bcff4703a
flavioh01 <149084095+flavioh01@users.noreply.github.com>
2024-09-05 16:00:27 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('hidden');
+}
+
+function toggleBackground() {
+    document.body.classList.toggle('black-bg');
+    document.body.classList.toggle('white-bg');
+}
+
+
